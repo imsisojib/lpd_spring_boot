9e4fbf14a7798bb7818bf7e276b4105485634d97
sojib-tikweb <sojib@tikweb.com>
2023-01-24 13:56:56 +0600

    init commit.

---
+++ src/main/java/com/imsisojib/lpd/LostPhoneDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.imsisojib.lpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostPhoneDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostPhoneDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imsisojib/lpd/LostPhoneDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imsisojib.lpd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostPhoneDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
